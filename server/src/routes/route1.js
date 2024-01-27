964c71a3c53f4583b5b372ee9cbd1d512dae175e
Jtan99 <jtantan99@gmail.com>
2024-01-27 14:04:37 -0800

    adding test route

--- server/src/routes/route1.js
+++ server/src/routes/route1.js
@@ -1,17 +1,10 @@
 // src/routes/route1.js
 const express = require('express');
-const { pgPool } = require('../config/db');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  try {
-    const result = await pgPool.query('SELECT * FROM your_pg_table');
-    res.json(result.rows);
-  } catch (error) {
-    console.error('Error querying PostgreSQL:', error);
-    res.status(500).send('Internal Server Error');
-  }
+  res.send('Hello from route1');
 });
 
 module.exports = router;
